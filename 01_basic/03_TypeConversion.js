b19e81d3ecf7e6ded83c96bc8a0a30d4f9891150
Tejas-Shete28 <Tejasshete@outlook.com>
2026-07-04 18:50:51 +0000

    Conversion of Datatypes

---
+++ 01_basic/03_TypeConversion.js
@@ -0,0 +1,69 @@
+let score = 28
+
+console.log("score: " + score)
+console.log(typeof score)
+console.log(typeof(score))
+
+score= "28tej"
+console.log("score: " + score)
+console.log(typeof score)
+
+let scoreInNumber = Number(score)
+console.log("scoreInNumber: " + scoreInNumber)
+console.log(typeof scoreInNumber)
+
+score1= null
+console.log("score1: " + score1)
+console.log(typeof score1)
+
+let scoreInNumber1 = Number(score1)
+console.log("scoreInNumber1: " + scoreInNumber1)
+console.log(typeof scoreInNumber1)
+
+let score2= undefined
+console.log("score2: " + score2)
+console.log(typeof score2)
+    
+let scoreInNumber2 = Number(score2)
+console.log("scoreInNumber2: " + scoreInNumber2)
+console.log(typeof scoreInNumber2)
+
+score3= true
+console.log("score3: " + score3)
+console.log(typeof score3)
+
+let scoreInNumber3 = Number(score3)
+console.log("scoreInNumber3: " + scoreInNumber3)
+console.log(typeof scoreInNumber3)
+
+let loggedIn = 1
+console.log("loggedIn: " + loggedIn)
+console.log(typeof loggedIn)
+
+let loggedInInBoolean = Boolean(loggedIn)
+console.log("loggedInInBoolean: " + loggedInInBoolean)
+console.log(typeof loggedInInBoolean)
+
+loggedIn = ""
+console.log("loggedIn: " + loggedIn)
+console.log(typeof loggedIn)
+
+let loggedInInBoolean1 = Boolean(loggedIn)
+console.log("loggedInInBoolean1: " + loggedInInBoolean1)
+console.log(typeof loggedInInBoolean1)
+
+let loggedIn2 = "tej"
+console.log("loggedIn2: " + loggedIn2)
+console.log(typeof loggedIn2)
+
+let loggedInInBoolean2 = Boolean(loggedIn2)
+console.log("loggedInInBoolean2: " + loggedInInBoolean2)
+console.log(typeof loggedInInBoolean2)
+
+loggedIn2 = undefined
+console.log("loggedIn2: " + loggedIn2)
+console.log(typeof loggedIn2)
+
+let loggedInInBoolean3 = Boolean(loggedIn2)
+console.log("loggedInInBoolean3: " + loggedInInBoolean3)
+console.log(typeof loggedInInBoolean3)
