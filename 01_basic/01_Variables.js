dea9e235dcc52e49b4c4796f3ef1661cbe6693b4
Tejas-Shete28 <Tejasshete@outlook.com>
2026-07-04 12:54:38 +0000

    variables

---
+++ 01_basic/01_Variables.js
@@ -0,0 +1,8 @@
+const accountID = 123456789;
+let accountBalance = 1000.50;
+var accountHolderName = "John Doe";
+accountCity = "Pune"; // This will not throw an error because accountCity is declared with let
+
+let State;
+
+console.table([accountID, accountBalance, accountHolderName, accountCity, State]);
